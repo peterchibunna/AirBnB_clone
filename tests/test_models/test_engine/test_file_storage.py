491e7a06d76ebd8f34122ceae97c404f2ba44d60
Peter Chibunna <peterchibunna@gmail.com>
2022-11-28 09:50:49 +0100

    More tests for storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,11 +2,17 @@
 """
 Test for storage
 """
-from datetime import datetime
 import unittest
-from time import sleep
-import json
+import os
 from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
@@ -23,5 +29,89 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsNotNone(FileStorage.save)
         self.assertIsNotNone(FileStorage.reload)
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_file_path(self):
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_objects(self):
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_all(self):
+        self.assertEqual(dict, type(storage.all()))
+
+    def test_new(self):
+        base_model = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+        storage.new(base_model)
+        storage.new(user)
+        storage.new(state)
+        storage.new(place)
+        storage.new(city)
+        storage.new(amenity)
+        storage.new(review)
+        self.assertIn("BaseModel." + base_model.id, storage.all().keys())
+        self.assertIn(base_model, storage.all().values())
+        self.assertIn("User." + user.id, storage.all().keys())
+        self.assertIn(user, storage.all().values())
+        self.assertIn("State." + state.id, storage.all().keys())
+        self.assertIn(state, storage.all().values())
+        self.assertIn("Place." + place.id, storage.all().keys())
+        self.assertIn(place, storage.all().values())
+        self.assertIn("City." + city.id, storage.all().keys())
+        self.assertIn(city, storage.all().values())
+        self.assertIn("Amenity." + amenity.id, storage.all().keys())
+        self.assertIn(amenity, storage.all().values())
+        self.assertIn("Review." + review.id, storage.all().keys())
+        self.assertIn(review, storage.all().values())
+
+    def test_save(self):
+        base_model = BaseModel()
+        user = User()
+        state = State()
+        place = Place()
+        city = City()
+        amenity = Amenity()
+        review = Review()
+
+        storage.new(base_model)
+        storage.new(user)
+        storage.new(state)
+        storage.new(place)
+        storage.new(city)
+        storage.new(amenity)
+        storage.new(review)
+        storage.save()
+
+        with open("file.json", "r") as f:
+            save_text = f.read()
+            self.assertIn("BaseModel." + base_model.id, save_text)
+            self.assertIn("User." + user.id, save_text)
+            self.assertIn("State." + state.id, save_text)
+            self.assertIn("Place." + place.id, save_text)
+            self.assertIn("City." + city.id, save_text)
+            self.assertIn("Amenity." + amenity.id, save_text)
+            self.assertIn("Review." + review.id, save_text)
+
+    def test_reload(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        with open("file.json", "w") as f:
+            f.write("{}")
+        with open("file.json", "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(a_storage.reload(), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
